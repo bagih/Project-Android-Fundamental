b798c9f223dcea79b6ffd707027925f91453077b
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-01 12:54:20 +0800

    Finishing Localization Example app v1

--- LocalizationExample/README.md
+++ LocalizationExample/README.md
@@ -2,3 +2,24 @@
 
 ## Tentang project
 > project ini mendemonstrasikan penggunaan localization dalam android
+
+>localization berkerja dengan cara membuat versi lain dari file original yang ingin di transalasikan,
+> versi tersebut biasanya menggunakan qualifier tambahan. pada project ini qualifier yang digunakan
+> adalah **qualifier** **locale**
+> 
+> ada **3** qualifier locale yang digunakan yaitu **IN (Bahasa), EN (English), JA (Japanese)**
+
+
+## Screenshot Aplikasi
+
+. **Tampilan dengan bahasa Inggris dan default**
+
+[<img src="./images/en.jpg" height="400"/>](images/en.jpg)
+
+. **Tampilan dengan bahasa Indonesia**
+
+[<img src="./images/id.jpg" height="400"/>](images/id.jpg)
+
+. **Tampilan dengan bahasa Jepang**
+
+[<img src="./images/jp.jpg" height="400"/>](images/jp.jpg)
--- LocalizationExample/app/src/main/java/com/bagih/localizationexample/MainActivity.kt
+++ LocalizationExample/app/src/main/java/com/bagih/localizationexample/MainActivity.kt
@@ -3,13 +3,22 @@ package com.bagih.localizationexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.AsyncImage
 import com.bagih.localizationexample.ui.theme.LocalizationExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomePage()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LocalizationExampleTheme {
-        Greeting("Android")
+        HomePage()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BodyPage(modifier: Modifier = Modifier) {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding() + 8.dp,
+                    end = paddingValues.calculateEndPadding(layoutDirection = LayoutDirection.Ltr) + 8.dp,
+                    bottom = paddingValues.calculateBottomPadding() + 8.dp,
+                    start = paddingValues.calculateStartPadding(layoutDirection = LayoutDirection.Ltr) + 8.dp
+
+                )
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.title),
+                modifier = modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = MaterialTheme.typography.headlineMedium.fontSize
+            )
+            Spacer(modifier = modifier.height(16.dp))
+            AsyncImage(
+                model = R.drawable.benang_kelambu,
+                contentDescription = stringResource(id = R.string.title),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp),
+
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextBody(content = stringResource(id = R.string.description_p1))
+            Spacer(modifier = Modifier.height(8.dp))
+            TextBody(content = stringResource(id = R.string.description_p2))
+            Spacer(modifier = Modifier.height(8.dp))
+            TextBody(content = stringResource(id = R.string.description_p3))
+        }
+    }
+}
+
+@Composable
+fun TextBody(modifier: Modifier = Modifier, content: String) {
+    Text(
+        text = content,
+        textAlign = TextAlign.Justify,
+        modifier = modifier.padding(horizontal = 8.dp)
+    )
+}
+
+@Composable
+fun HomePage() {
+    BodyPage()
 }
